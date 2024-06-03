7360a1be8b13c73e7acf9a811c4632d8fad6b98c
gope-gope <georgigeorgiew97@gmail.com>
2024-06-03 16:41:57 +0300

    Fix: use wagmi provider globally

--- Front-End/src/Components/config.ts
+++ Front-End/src/Components/config.ts
@@ -1,10 +1,9 @@
-import { http, createConfig } from 'wagmi'
-import { linea } from 'wagmi/chains'
+import { http, createConfig } from "wagmi";
+import { linea } from "wagmi/chains";
 
 export const config = createConfig({
   chains: [linea],
   transports: {
     [linea.id]: http(),
   },
-})
-// export{}
+});
--- Front-End/src/index.tsx
+++ Front-End/src/index.tsx
@@ -3,13 +3,17 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { WagmiProvider } from 'wagmi';
+import { config } from './Components/config';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
+    <WagmiProvider config={config}>
     <App />
+    </WagmiProvider>
   </React.StrictMode>
 );
 
